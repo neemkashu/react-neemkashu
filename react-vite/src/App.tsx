63763dc06817d2eb066911a4fdaa18881358f41c
neemkashu <sparjinkust@mail.ru>
2023-03-09 14:26:49 +0300

    chore: add configs from the task

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-  const a = 1;
 
   return (
     <div className="App">
