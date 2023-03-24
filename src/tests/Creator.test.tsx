8ac2dccdcf7aa601923e737e1cb53f21b39b188c
neemkashu <sparjinkust@mail.ru>
2023-03-24 23:34:30 +0300

    test: add test for adding a pet card

--- src/tests/Creator.test.tsx
+++ src/tests/Creator.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { Creator } from '../components/Creator';
 import { PetCardTextContent } from '../utils/constants';
+import { fillForm } from './PetForm.test';
 
 describe('Creator component', () => {
   it('Renders form', () => {
@@ -24,5 +25,17 @@ describe('Creator component', () => {
       const cardsNew = screen.queryAllByText(PetCardTextContent.petBirth);
       expect(cardsNew.length).toBe(previousCardAmount);
     }
+  });
+  it('Renders card if submit correct data', async () => {
+    URL.createObjectURL = vi.fn().mockReturnValue('mock-url');
+    render(<Creator />);
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+    await fillForm();
+
+    const user = userEvent.setup();
+    await user.click(submitButton);
+
+    const petCard = screen.getByText('Aname');
+    expect(petCard).toBeInTheDocument();
   });
 });
