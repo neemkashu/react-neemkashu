ab580e576e2e9546c767761b9dac6a81693316ed
neemkashu <sparjinkust@mail.ru>
2023-03-30 13:45:31 +0300

    refactor: replace components without side effects with functional components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,7 @@
-import { Component } from 'react';
-
-export class About extends Component {
-  render() {
-    return (
-      <div className="sm:p-2 p-1">
-        <h2 className="font-bold text-yellow-800 text-2xl">React app created with Vite</h2>
-      </div>
-    );
-  }
-}
+export const About = () => {
+  return (
+    <div className="sm:p-2 p-1">
+      <h2 className="font-bold text-yellow-800 text-2xl">React app created with Vite</h2>
+    </div>
+  );
+};
--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,12 +1,8 @@
-import { Component } from 'react';
-
-export class ErrorPage extends Component {
-  render() {
-    return (
-      <div className="my-1 mx-auto">
-        <p className="text-center text-2xl">Oops!</p>
-        <p className="text-center text-1xl">Page not found</p>
-      </div>
-    );
-  }
-}
+export const ErrorPage = () => {
+  return (
+    <div className="my-1 mx-auto">
+      <p className="text-center text-2xl">Oops!</p>
+      <p className="text-center text-1xl">Page not found</p>
+    </div>
+  );
+};
