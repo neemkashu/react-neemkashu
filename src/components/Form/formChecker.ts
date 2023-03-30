6e2b799f4074f8102000039fbc9e5135c9adb98a
neemkashu <sparjinkust@mail.ru>
2023-03-31 01:40:01 +0300

    refactor: correct grammar of the errors messages

--- src/components/Form/formChecker.ts
+++ src/components/Form/formChecker.ts
@@ -1,3 +1,5 @@
+import { mapOverObject } from '../../utils/helpers';
+
 export type PetFormData = {
   name: string;
   birth: string;
@@ -11,40 +13,49 @@ export type FieldMessages = Record<keyof PetFormData, string>;
 export type FieldVerdicts = Record<keyof PetFormData, boolean>;
 
 export const ErrorMessages: FieldMessages = {
-  name: 'enter a name with first uppercase letter',
-  birth: 'choose date of birth',
-  type: 'choose type of the pet',
-  sex: 'choose male or female',
-  isExperienced: 'confirm that you have read the rules of the show',
-  img: 'choose file with a picture',
+  name: 'Enter a name with the first uppercase letter.',
+  birth: 'Choose a date of birth that is earlier than today.',
+  type: 'Choose the type of pet.',
+  sex: 'Choose male or female.',
+  isExperienced: 'Confirm that you have read the rules of the show',
+  img: 'Choose a file with a picture',
 } as const;
+
+const vilidateBirth = (birth: string): boolean => {
+  if (!birth) return !!birth;
+  const date = new Date(birth);
+  const today = new Date();
+  return date.getTime() < today.getTime();
+};
 
 const getValidationVerdicts = (formData: PetFormData): FieldVerdicts => {
   const { name, birth, type, sex, img, isExperienced } = formData;
+
   const validations = {
     name: name !== '' && name[0].toLowerCase() !== name[0],
-    birth: birth !== '',
+    birth: vilidateBirth(birth),
     type: type !== '',
     sex: sex !== '',
     isExperienced,
-    img: img !== null && img.length > 0,
+    img: !!img?.length,
   };
+
   return validations;
 };
 
 export const checkFormIsValid = (formData: PetFormData): boolean => {
   const validations = getValidationVerdicts(formData);
+
   return Object.values(validations).every((validValue) => validValue);
 };
 
 export const getErrorMessages = (formData: PetFormData): FieldMessages => {
   const validations = getValidationVerdicts(formData);
 
-  const messages = Object.keys(ErrorMessages).reduce<FieldMessages>((accum, key) => {
-    const keyErr = key as keyof FieldMessages;
+  const messages = mapOverObject(ErrorMessages, (accum, keyErr) => {
     accum[keyErr] = validations[keyErr] ? '' : ErrorMessages[keyErr];
     return accum;
-  }, {} as FieldMessages);
+  });
 
   return messages;
 };
