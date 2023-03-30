f6bc8ce9b14af4461222f2b3c41df51201ef91e8
neemkashu <sparjinkust@mail.ru>
2023-03-30 14:39:39 +0300

    refactor: remove magic number from Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,13 @@ import { useMatches } from 'react-router-dom';
 import { RoutesInfo } from '../../src/utils/constants';
 import { NavigationBar } from './NavigationBar';
 
+const PAGE_ROUTE_DEPTH = 1;
+
 const Header = () => {
   const matches = useMatches();
-  const title = Object.values(RoutesInfo).find(({ path }) => path === matches[1].pathname)?.header;
+  const title = Object.values(RoutesInfo).find(
+    ({ path }) => path === matches[PAGE_ROUTE_DEPTH].pathname
+  )?.header;
 
   return (
     <header className="flex justify-between items-center pr-2 bg-orange-400">
