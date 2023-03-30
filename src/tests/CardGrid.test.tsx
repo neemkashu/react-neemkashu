225f6895407b1d0b837ea13e9488c8c9701559ef
neemkashu <sparjinkust@mail.ru>
2023-03-31 00:58:53 +0300

    fix: update path in CardGrid test

--- src/tests/CardGrid.test.tsx
+++ src/tests/CardGrid.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
-import { CardGrid } from 'src/layouts/CardGrid';
+import { CardGrid } from '../layouts/CardGrid';
 
 describe('Grid component', () => {
   it('Renders grid children', () => {
