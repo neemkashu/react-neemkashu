ce4b13122f5ac31697e0844abc5f409a50f5fe56
neemkashu <sparjinkust@mail.ru>
2023-04-20 11:08:42 +0300

    test: add server error test

--- src/components/Search/SearchError.tsx
+++ src/components/Search/SearchError.tsx
@@ -1,7 +1,5 @@
 import { FC } from 'react';
-import { useAsyncError } from 'react-router-dom';
 
 export const SearchError: FC<Record<string, never>> = () => {
-  const error = useAsyncError() as { message?: string };
-  return <p>Error loading photos 😥! {error?.message}</p>;
+  return <p>Error loading photos 😥!</p>;
 };
--- src/tests/SearchController.test.tsx
+++ src/tests/SearchController.test.tsx
@@ -1,35 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { createMemoryRouter, RouteObject, RouterProvider } from 'react-router-dom';
 import { setupServer } from 'msw/node';
+import { rest } from 'msw';
 import { Provider } from 'react-redux';
-import { describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
 import { RoutesInfo } from '../utils/constants';
 import { handlers } from '../mocks/apiHandlers';
 import { SearchController } from '../components/Search/SearchController';
 import { store } from '../redux/store';
+import { PROXY_URL } from '../api/getCards';
+import { flickrApi } from '../api/flickrApi';
 
 const server = setupServer(...handlers);
 
-const routesMock: RouteObject[] = [
-  {
-    path: RoutesInfo.MAIN.path,
-    element: (
-      <Provider store={store}>
-        <SearchController />
-      </Provider>
-    ),
-  },
-];
-
 describe('Search controller', () => {
+  const routesMock: RouteObject[] = [
+    {
+      path: RoutesInfo.MAIN.path,
+      element: (
+        <Provider store={store}>
+          <SearchController />
+        </Provider>
+      ),
+    },
+  ];
+  const router = createMemoryRouter(routesMock, {
+    initialEntries: [RoutesInfo.MAIN.path],
+  });
   beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
+  afterEach(() => {
+    server.resetHandlers();
+
+    act(() => {
+      store.dispatch(flickrApi.util.resetApiState());
+    });
+  });
   afterAll(() => server.close());
 
   it('Renders search', async () => {
-    const router = createMemoryRouter(routesMock, {
-      initialEntries: [RoutesInfo.MAIN.path],
-    });
     render(<RouterProvider router={router} />);
 
     const input = await screen.findByRole('searchbox');
@@ -37,13 +45,24 @@ describe('Search controller', () => {
     expect(input).toBeInTheDocument();
   });
   it('Renders cards', async () => {
-    const router = createMemoryRouter(routesMock, {
-      initialEntries: [RoutesInfo.MAIN.path],
-    });
     render(<RouterProvider router={router} />);
 
     const cardButtons = await screen.findAllByText('Details');
 
     expect(cardButtons[0]).toBeInTheDocument();
+  });
+
+  it('Renders error element if error response', async () => {
+    server.use(
+      rest.get(PROXY_URL, (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: 'Internal server error' }));
+      })
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const error = await screen.findByText(/error/i);
+
+    expect(error).toBeInTheDocument();
   });
 });
