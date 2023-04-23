ff005bd6f9261801f2bb1f078524255ce7e17562
neemkashu <sparjinkust@mail.ru>
2023-04-23 23:25:29 +0300

    refactor: remove routes duplication

--- src/routesConfig.tsx
+++ src/routesConfig.tsx
@@ -1,4 +1,11 @@
-import { Navigate, Outlet, Route, RouteObject, Routes } from 'react-router-dom';
+import {
+  createRoutesFromChildren,
+  Navigate,
+  Outlet,
+  Route,
+  RouteObject,
+  Routes,
+} from 'react-router-dom';
 import { FC } from 'react';
 import Header from './components/Header';
 import { AboutPage } from './pages/AboutPage';
@@ -7,82 +14,44 @@ import { MainPage } from './pages/MainPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { RoutesInfo } from './utils/constants';
 
-export const routesConfig: RouteObject[] = [
-  {
-    path: RoutesInfo.MAIN.path,
-    element: (
+const AppRoutes = (
+  <Route
+    element={
       <>
         <Header />
         <Outlet />
       </>
-    ),
-    children: [
-      {
-        path: RoutesInfo.MAIN.path,
-        element: <MainPage />,
-      },
-      {
-        path: RoutesInfo.ABOUT.path,
-        element: <AboutPage />,
-      },
-      {
-        path: RoutesInfo.FORM.path,
-        element: <FormPage />,
-      },
-      {
-        path: RoutesInfo.NOT_FOUND.path,
-        element: <NotFoundPage />,
-      },
-      {
-        path: '*',
-        element: (
-          <Navigate
-            to={RoutesInfo.NOT_FOUND.path}
-            replace
-          />
-        ),
-      },
-    ],
-  },
-];
+    }
+  >
+    <Route
+      path={RoutesInfo.MAIN.path}
+      element={<MainPage />}
+    />
+    <Route
+      path={RoutesInfo.ABOUT.path}
+      element={<AboutPage />}
+    />
+    <Route
+      path={RoutesInfo.FORM.path}
+      element={<FormPage />}
+    />
+    <Route
+      path={RoutesInfo.NOT_FOUND.path}
+      element={<NotFoundPage />}
+    />
+    <Route
+      path="*"
+      element={
+        <Navigate
+          to={RoutesInfo.NOT_FOUND.path}
+          replace
+        />
+      }
+    />
+  </Route>
+);
+export const routesConfig: RouteObject[] = createRoutesFromChildren(AppRoutes);
 
 export const App: FC<Record<string, never>> = () => {
-  return (
-    <Routes>
-      <Route
-        element={
-          <>
-            <Header />
-            <Outlet />
-          </>
-        }
-      >
-        <Route
-          path={RoutesInfo.MAIN.path}
-          element={<MainPage />}
-        />
-        <Route
-          path={RoutesInfo.ABOUT.path}
-          element={<AboutPage />}
-        />
-        <Route
-          path={RoutesInfo.FORM.path}
-          element={<FormPage />}
-        />
-        <Route
-          path={RoutesInfo.NOT_FOUND.path}
-          element={<NotFoundPage />}
-        />
-        <Route
-          path="*"
-          element={
-            <Navigate
-              to={RoutesInfo.NOT_FOUND.path}
-              replace
-            />
-          }
-        />
-      </Route>
-    </Routes>
-  );
+  return <Routes>{AppRoutes}</Routes>;
 };
