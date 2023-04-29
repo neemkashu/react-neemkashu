b1cc80bc55a92054cf96777ada9e6325f25586a6
neemkashu <sparjinkust@mail.ru>
2023-04-29 19:02:02 +0300

    test: add cypress coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'cypress';
+import coverage from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+    },
+  },
   e2e: {
+    // experimentalStudio: true,
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      coverage(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
+  },
+  video: false,
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,46 @@
 describe('template spec', () => {
+  const PROXY_URL = 'https://sore-plum-skunk-wig.cyclic.app';
+
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'GET', // Route all GET requests
+        url: PROXY_URL, // that have a URL that matches '/users/*'
+      },
+      [] // and force the response to be: []
+    ).as('getPhotos'); // and assign an alias
+  });
+
   it('passes', () => {
-    cy.visit(' http://localhost:5173');
+    cy.visit(' http://localhost:5173/form');
 
     cy.contains(/about/i).click();
     cy.url().should('include', '/about');
   });
 
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Fetch initial card and new query', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('localhost:5173/about');
+    cy.get('.flex > :nth-child(1) > .p-1').click();
+    cy.get('p').should('be.visible');
+    cy.get('.grid > :nth-child(1)', { timeout: 8000 }).should('be.visible');
+    cy.get('._input_1y23s_1').type('bird');
+    cy.get('.flex-row > .self-center').click();
+    cy.get('.grid > :nth-child(1)', { timeout: 5000 }).should('be.visible');
+    /* ==== End Cypress Studio ==== */
+  });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('Fetch info for modal', function () {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('localhost:5173/');
+    cy.get(':nth-child(1) > .flex-wrap > .flex-col > .self-center').click();
+    cy.get('.m-auto > .flex-wrap', { timeout: 5000 }).should('be.visible');
+    cy.get('.fixed').should('be.visible');
+    cy.get('.self-end').click();
+    cy.get(':nth-child(2) > .flex-wrap > .flex-col > .self-center').click();
+    cy.get('.m-auto > .flex-wrap', { timeout: 5000 }).should('be.visible');
+    /* ==== End Cypress Studio ==== */
   });
 });
